78cfc46437a264db33bfe7be6ac18e4537fea0b6
You Song Hou <ysongweb3@gmail.com>
2023-07-26 22:46:35 -0400

    feat: install 'react-router-dom'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import Home from './pages/Home';
@@ -15,11 +16,23 @@ function App() {
   }
   return (
     <ChakraProvider>
-      <h1>First Project with Sway</h1>
-      <button onClick={handleClick}>Create Wallet</button>
-      {address && <p>Address: {address}</p>}
-      {privateKey && <p>Private Key: {privateKey}</p>}
-      <Home />
+      <HashRouter>
+        <Routes>
+          <Route
+            path="/wallet"
+            element={
+              <>
+                <h1>First Project with Sway</h1>
+                <button onClick={handleClick}>Create Wallet</button>
+                {address && <p>Address: {address}</p>}
+                {privateKey && <p>Private Key: {privateKey}</p>}
+              </>} />
+          <Route
+            path="/"
+            element={
+              <Home />} />
+        </Routes>
+      </HashRouter>
     </ChakraProvider>
   )
 }
